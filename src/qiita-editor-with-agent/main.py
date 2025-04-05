6bf0d0b34b2d836a5abe9dcc6f59da14cc77db51
u-masao <4973920+u-masao@users.noreply.github.com>
2025-04-06 00:08:50 +0900

    load_dotenv() を利用。Qiita 以外のプラットフォーム指定でエラーを投げる。ユーザーへ確認しないようにプロンプトを修正。コメントを追加。アウトプットの文言を詳細化。UI のレイアウトを調整。

--- src/qiita-editor-with-agent/main.py
+++ src/qiita-editor-with-agent/main.py
@@ -1,28 +1,18 @@
 """
-このモジュールは、Qiita に投稿する原稿の内容を AI エージェントが
-審査して投稿するツールのエントリポイントです。
+# Qiita Editor with Agent
 
-TODO:
+AI エージェントがブログの原稿をチェックして投稿してくれるエディタ
 
-- [x] ブラウザで動作する UI をもつ
-- [x] 人間が執筆した記事を受け付ける画面を持つ
-    - [x] UI で記事を編集できるようにする
-    - [x] Markdown をレンダリングしたプレビューを表示する
-    - [ ] 外部エディタで編集できるようにファイルから読み取る機能もほしい
-- [x] 人間から審査プロセス開始の合図を受ける
-- [x] 記事のチェックが通ったら人間に承認を依頼する
-- [ ] 人間が承認しなかったら動作を停止する
-- [x] 人間の承認は問わずに媒体に投稿する
-- [x] 人間が承認したら媒体に投稿する
-    - [ ] 公開モード：媒体に投稿してすぐに公開する
-    - [x] 非公開モード：媒体に投稿するけど公開しない
-- [x] 投稿したら投稿結果を表示する
-    - [x] 結果の例：正常異常のステータス、URL
+サポートする媒体: Qiita
 """
 
 import json
 import os
 from typing import Any, List, Literal, Tuple
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import gradio as gr
 from agents import Agent, RunContextWrapper, Runner, function_tool
@@ -32,7 +22,7 @@ from qiita.v2.models.create_item_request import CreateItemRequest
 from qiita.v2.models.item_tag import ItemTag
 
 EDITOR_LINES = os.getenv("EDITOR_LINES", 30)
-PREVIEW_HEIGHT = os.getenv("PREVIEW_HEIGHT", 700)
+PREVIEW_HEIGHT = os.getenv("PREVIEW_HEIGHT", 600)
 MAX_ARTICLE_TAGS = os.getenv("MAX_ARTICLE_TAGS", 5)
 
 
@@ -51,7 +41,7 @@ class ArticleInformation(BaseModel):
 
 class CheckResult(BaseModel):
     """
-    編集者とチェック者が返すメッセージフォーマット
+    編集者とチェック者が返すメッセージのフォーマット
     """
 
     status: Literal["reject", "accept"]
@@ -74,16 +64,29 @@ async def confirm_to_human(
 
 
 @function_tool
-def publish_to_platform(run_ctx: RunContextWrapper[Any]) -> str:
+def publish_to_platform(
+    run_ctx: RunContextWrapper[Any], platform: Literal["qiita"]
+) -> str:
     """
     審査済みの記事を媒体へ投稿する関数
+
+    Args:
+        platform: 投稿先の媒体名, qiita をサポート
     """
-    article_info: ArticleInformation = run_ctx.context
-    qiita = Qiita(access_token=os.environ["QIITA_API_ACCESS_TOKEN"])
-    response = qiita.create_item_with_http_info(
-        CreateItemRequest(**article_info.model_dump())
-    )
-    return response.data
+
+    if platform not in ["qiita"]:
+        raise ValueError(f"その媒体はサポートしていません: {platform}")
+
+    if platform == "qiita":
+        # Qiita に投稿する処理
+        article_info: ArticleInformation = run_ctx.context
+        qiita = Qiita(access_token=os.environ["QIITA_API_ACCESS_TOKEN"])
+        response = qiita.create_item_with_http_info(
+            CreateItemRequest(**article_info.model_dump())
+        )
+        result = response.data
+
+    return result
 
 
 # 公開責任者エージェント
@@ -91,15 +94,20 @@ applover_agent = Agent(
     name="公開責任者",
     instructions="""
     あなたは記事を媒体に投稿する前に公開の可否を最終判断する公開責任者です。
-    毎回、confirm_to_human 関数を使って第三者に判断を仰いでください。
-    第三者が accept と判断した場合は、publish_to_platform 関数を呼び出して
+    執筆者や編集者から受け取る記事を媒体に投稿すべきかを判断します。
+    絶対にユーザー、執筆者、編集者へ意見を聞かないでください。
+
+    必ず confirm_to_human 関数を使って第三者に判断を仰いでください。
+    第三者が accept と判断した場合は publish_to_platform 関数を呼び出して
     記事を投稿してください。
-    第三者が reject と判断した場合は、reject した旨を応答してください。
-    ユーザーに対しては判断結果と投稿したURLを返してください。
+    第三者が reject と判断した場合は reject した旨を応答してください。
+
+    ユーザーに対しては判断結果、判断結果の根拠、投稿記事の URL を返してください。
+    絶対にユーザー、執筆者、編集者へ意見を聞かないでください。
     """,
     tools=[
-        confirm_to_human,
-        publish_to_platform,
+        confirm_to_human,  # 人間に最終確認するツール
+        publish_to_platform,  # 媒体に投稿するツール
     ],
 )
 
@@ -162,18 +170,24 @@ editor_agent = Agent(
     """,
     handoffs=[applover_agent],
     tools=[
-        readability_check_tool,
-        quailty_check_tool,
-        code_format_check_tool,
+        readability_check_tool,  # 読みやすさをチェックするエージェント
+        quailty_check_tool,  # 品質をチェックするエージェント
+        code_format_check_tool,  # コードをチェックするエージェント
     ],
 )
 
 
-async def review_and_post(title, tags, body, chat_history):
+async def review_and_post(
+    title, tags, body, private_flag, slide_flag, chat_history
+):
     """
     記事のレビュー、投稿確認、投稿作業を行う。結果をチャットの履歴で返す。
     """
-    chat_history.append({"role": "user", "content": f"「{title}」を審査して"})
+    chat_history += [
+        {"role": "user", "content": f"「{title}」を Qiita に投稿して"},
+        {"role": "assistant", "content": "はい、チェックしてから投稿しますね"},
+        {"role": "assistant", "content": "・・・"},
+    ]
     yield chat_history
 
     def input_validation_check(
@@ -181,12 +195,12 @@ async def review_and_post(title, tags, body, chat_history):
     ) -> Tuple[bool, str]:
         """入力チェック"""
         if len(title) == 0:
-            return False, "タイトルが空です"
+            return False, "タイトルが空です。タイトルを入力してください。"
         if len(body) == 0:
-            return False, "本文が空です"
+            return False, "本文が空です。本文を入力してください。"
         if len(tags.strip().split(" ")) > MAX_ARTICLE_TAGS:
             return False, "タグが多すぎます。5 以下にしてください。"
-        return True, "問題ありません"
+        return True, "問題ありません。"
 
     # 入力チェック
     valid_flag, message = input_validation_check(title, tags, body)
@@ -203,20 +217,26 @@ async def review_and_post(title, tags, body, chat_history):
                 for x in str(tags).strip().split(" ")
             ],
             body=body,
+            private=private_flag,
+            slide=slide_flag,
         )
 
         # エージェントを実行
+        # 記事の情報と投稿先をプロンプトとして渡す(JSON形式)
+        # 途中で LLM に編集されるのを避けるため記事の text は context で渡す
         response = await Runner.run(
-            editor_agent,
-            input=json.dumps(
+            editor_agent,  # 編集者エージェントに依頼
+            input=json.dumps(  # LLM には JSON 形式で情報を渡す
                 {
+                    "platform": "qiita",
                     "title": title,
                     "tags": tags,
                     "body": body,
+                    "slide_flag": slide_flag,
                 },
-                ensure_ascii=False,
+                ensure_ascii=False,  # 漢字等をエスケープさせない
             ),
-            context=article_info,
+            context=article_info,  # Tool に渡す情報
         )
 
         # エージェントの出力をチャットに追加
@@ -227,32 +247,77 @@ async def review_and_post(title, tags, body, chat_history):
         yield chat_history
 
 
+HEADER_TEXT = """
+### AI エージェントが Qiita の原稿をチェックして投稿してくれるエディタ
+"""
+
+
 with gr.Blocks() as demo:
+    # UI コンポーネントを配置
+    preview_kwargs = {
+        "height": PREVIEW_HEIGHT,
+        "line_breaks": True,
+        "show_label": False,
+        "value": "# プレビュー\n\nここに本文のプレビューが表示されます",
+    }
+    gr.Markdown(HEADER_TEXT, show_label=False)
     with gr.Row():
         with gr.Column():
-            title_pane = gr.Textbox(lines=1, show_label=False)
-            tags_pane = gr.Textbox(lines=1, show_label=False)
-            editor_pane = gr.Textbox(lines=EDITOR_LINES, show_label=False)
-        preview_pane = gr.Markdown(height=PREVIEW_HEIGHT)
-        chat_history_pane = gr.Chatbot(type="messages", height=PREVIEW_HEIGHT)
-    submit = gr.Button("チェックお願いします！")
+            with gr.Accordion("タイトル、公開設定", open=False):
+                title_pane = gr.Textbox(lines=1, max_lines=1, label="タイトル")
+                tags_pane = gr.Textbox(
+                    lines=1, max_lines=1, label="タグ(5個まで)"
+                )
+                private_flag = gr.Checkbox(value=True, label="限定公開")
+                slide_flag = gr.Checkbox(value=False, label="スライドモード")
+            body_pane = gr.Textbox(
+                lines=EDITOR_LINES,
+                show_label=True,
+                autofocus=True,
+                label="本文",
+            )
+        with gr.Tab("メッセージ"):
+            welcome_message = {
+                "role": "assistant",
+                "content": """
+                原稿を入力したら「チェック＆投稿」ボタンを押してください。
+                内容に問題がなければ Qiita に投稿します。
+                """,
+            }
+            chat_history_pane = gr.Chatbot(
+                [welcome_message],
+                type="messages",
+                height=PREVIEW_HEIGHT,
+                show_label=False,
+            )
+            submit_button = gr.Button("チェック＆投稿")
+        with gr.Tab("プレビュー"):
+            preview_pane = gr.Markdown(**preview_kwargs)
 
     # markdown プレビューの更新
     gr.on(
-        [editor_pane.change],
-        fn=lambda x: gr.Markdown(x),
-        inputs=[editor_pane],
+        [body_pane.change],
+        fn=lambda x: gr.Markdown(x, **preview_kwargs),
+        inputs=[body_pane],
         outputs=[preview_pane],
     )
 
     # チェック&投稿依頼
     gr.on(
-        [submit.click],
+        [submit_button.click],
         fn=review_and_post,
-        inputs=[title_pane, tags_pane, editor_pane, chat_history_pane],
+        inputs=[
+            title_pane,
+            tags_pane,
+            body_pane,
+            private_flag,
+            slide_flag,
+            chat_history_pane,
+        ],
         outputs=[chat_history_pane],
     )
 
 
 if __name__ == "__main__":
+    # Gradio UI を起動
     demo.launch(share=False, debug=True)
